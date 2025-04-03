d3856fa1321dc89080eaa448a862a81d2dd013f5
Yusuf Ahmed <yusufahmed195@gmail.com>
2025-04-04 05:28:41 +0530

    fix: modify keybindings for neo-tree, buffer line and general keys, change theme

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,18 +7,18 @@ return {
   config = function()
     require('bufferline').setup {
       options = {
-        mode = 'buffers', -- set to "tabs" to only show tabpages instead
-        themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
-        numbers = 'none', -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-        close_command = 'Bdelete! %d', -- can be a string | function, see "Mouse actions"
+        mode = 'buffers', -- Set to "tabs" to only show tabpages instead
+        themable = true, -- Allows highlight groups to be overridden
+        numbers = 'none', -- Can be "ordinal" | "buffer_id" | "both"
+        close_command = 'Bdelete! %d', -- Close buffer command
         buffer_close_icon = '✗',
         close_icon = '✗',
-        path_components = 1, -- Show only the file name without the directory
+        path_components = 1, -- Show only file name
         modified_icon = '●',
         left_trunc_marker = '',
         right_trunc_marker = '',
         max_name_length = 30,
-        max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+        max_prefix_length = 30, -- Prefix used when buffer is de-duplicated
         tab_size = 21,
         diagnostics = false,
         diagnostics_update_in_insert = false,
@@ -26,20 +26,27 @@ return {
         show_buffer_icons = true,
         show_buffer_close_icons = true,
         show_close_icon = true,
-        persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-        separator_style = { '│', '│' }, -- | "thick" | "thin" | { 'any', 'any' },
+        persist_buffer_sort = true, -- Preserve buffer sorting
+        separator_style = { '│', '│' }, -- Can be "thick" | "thin" | { 'any', 'any' }
         enforce_regular_tabs = true,
         always_show_bufferline = true,
         show_tab_indicators = false,
         indicator = {
-          -- icon = '▎', -- this should be omitted if indicator style is not 'icon'
           style = 'none', -- Options: 'icon', 'underline', 'none'
         },
         icon_pinned = '󰐃',
         minimum_padding = 1,
         maximum_padding = 5,
         maximum_length = 15,
-        sort_by = 'insert_at_end',
+        sort_by = 'insert_at_end', -- Ensure this is a valid option for bufferline.nvim
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Nvim Tree",
+            separator = true,
+            text_align = "left"
+          }
+        },
       },
       highlights = {
         separator = {
@@ -49,12 +56,8 @@ return {
           bold = true,
           italic = false,
         },
-        -- separator_selected = {},
-        -- tab_selected = {},
-        -- background = {},
-        -- indicator_selected = {},
-        -- fill = {},
       },
     }
   end,
 }
+
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -135,25 +135,26 @@ return {
         mappings = {
           ['<space>'] = {
             'toggle_node',
-            nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+            nowait = true -- disable `nowait` if you have existing combos starting with this char that you want to use
           },
+
           ['<2-LeftMouse>'] = 'open',
           ['<cr>'] = 'open',
           ['<esc>'] = 'cancel', -- close preview or floating neo-tree window
-          ['P'] = { 'toggle_preview', config = { use_float = true } },
+          ['P'] = { 'toggle_preview' },
           ['l'] = 'open',
-          ['S'] = 'open_split',
-          ['s'] = 'open_vsplit',
-          -- ["S"] = "split_with_window_picker",
-          -- ["s"] = "vsplit_with_window_picker",
-          ['t'] = 'open_tabnew',
-          -- ["<cr>"] = "open_drop",
-          -- ["t"] = "open_tab_drop",
-          ['w'] = 'open_with_window_picker',
-          --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
-          ['C'] = 'close_node',
-          -- ['C'] = 'close_all_subnodes',
-          ['z'] = 'close_all_nodes',
+          --['S'] = 'open_split',
+          --['s'] = 'open_vsplit',
+          ---- ["S"] = "split_with_window_picker",
+          ---- ["s"] = "vsplit_with_window_picker",
+          --['t'] = 'open_tabnew',
+          ---- ["<cr>"] = "open_drop",
+          ---- ["t"] = "open_tab_drop",
+          --['w'] = 'open_with_window_picker',
+          ----["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+          --['C'] = 'close_node',
+          ---- ['C'] = 'close_all_subnodes',
+          --['z'] = 'close_all_nodes',
           --["Z"] = "expand_all_nodes",
           ['a'] = {
             'add',
@@ -163,13 +164,13 @@ return {
               show_path = 'none', -- "none", "relative", "absolute"
             },
           },
-          ['A'] = 'add_directory', -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+          -- ['A'] = 'add_directory', -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
           ['d'] = 'delete',
           ['r'] = 'rename',
-          ['y'] = 'copy_to_clipboard',
+          ['c'] = 'copy_to_clipboard',
           ['x'] = 'cut_to_clipboard',
           ['p'] = 'paste_from_clipboard',
-          ['c'] = 'copy', -- takes text input for destination, also accepts the optional config.show_path option like "add":
+          -- ['c'] = 'copy', -- takes text input for destination, also accepts the optional config.show_path option like "add":
           -- ["c"] = {
           --  "copy",
           --  config = {
@@ -192,16 +193,7 @@ return {
           hide_dotfiles = false,
           hide_gitignored = false,
           hide_hidden = false, -- only works on Windows for hidden files/directories
-          hide_by_name = {
-            '.DS_Store',
-            'thumbs.db',
-            'node_modules',
-            '__pycache__',
-            '.virtual_documents',
-            '.git',
-            '.python-version',
-            '.venv',
-          },
+          hide_by_name = {},
           hide_by_pattern = { -- uses glob style patterns
             --"*.meta",
             --"*/src/*/tsconfig.json",
@@ -309,7 +301,7 @@ return {
     }
 
     vim.cmd [[nnoremap \ :Neotree reveal<cr>]]
-    vim.keymap.set('n', '<leader>e', ':Neotree toggle position=left<CR>', { noremap = true, silent = true }) -- focus file explorer
+    vim.keymap.set('n', '<C-b>', ':Neotree toggle position=left<CR>', { noremap = true, silent = true }) -- focus file explorer
     vim.keymap.set('n', '<leader>ngs', ':Neotree float git_status<CR>', { noremap = true, silent = true }) -- open git status window
   end,
 }
